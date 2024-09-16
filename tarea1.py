aff0cdc0b7b1a3ad76e43eafde0cf8c93e3b66b2
Luis Miguel Camacho Orjuela <140006110+lumicaor07@users.noreply.github.com>
2024-09-16 10:01:32 -0500

    Add files via upload

--- tarea1.py
+++ tarea1.py
@@ -1,23 +1,32 @@
 import requests
-import pandas as pd
+from bs4 import BeautifulSoup
+import os
 
-# Reemplaza esto con el enlace directo al archivo CSV de la página de datos abiertos
-url = "https://datosabiertos.bogota.gov.co/dataset/584111e1-202c-4f17-8ad5-3ea890e5113c/resource/9ef92d4f-3d86-40cf-a1f3-220839dc2b9d/download/osb_enftransm_mpox23082024.csv"
-
-# Realizar la solicitud GET para obtener los datos
+# URL de la página principal
+url = 'https://datosabiertos.bogota.gov.co/dataset/mpox-en-bogota-d-c'
 response = requests.get(url)
 
-if response.status_code == 200:
-    # Guardar el contenido de la respuesta en un archivo CSV temporal
-    with open("mpox_bogota.csv", "wb") as file:
-        file.write(response.content)
+# Parsear el contenido HTML
+soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Leer los datos en un DataFrame de pandas desde el archivo temporal con codificación latin1
-    data = pd.read_csv("mpox_bogota.csv", encoding='latin1')
+# Encuentra todos los enlaces que cumplen con las características específicas
+links = soup.find_all('a', attrs={'class': 'btn btn-default background-blue block-right-width hidden-xs'})
 
-    # Guardar los datos en un archivo Excel
-    data.to_excel("mpox_en_bogota.xlsx", index=False)
+# Buscar el enlace que contiene la URL del archivo CSV
+for link in links:
+    if 'Previsualización' in link.text:
+        href = link.get('href')
+        full_url = 'https://datosabiertos.bogota.gov.co' + href
+        print('Enlace encontrado:', full_url)
+        
+        # Realizar la solicitud GET para descargar el archivo
+        file_response = requests.get('https://datosabiertos.bogota.gov.co/dataset/584111e1-202c-4f17-8ad5-3ea890e5113c/resource/9ef92d4f-3d86-40cf-a1f3-220839dc2b9d/download/osb_enftransm_mpox_06092024.csv')
 
-    print("Datos guardados exitosamente en 'mpox_en_bogota.xlsx'")
-else:
-    print(f"Error al obtener los datos: {response.status_code}")
+        # Comprobar que la solicitud fue exitosa
+        if file_response.status_code == 200:
+            # Guardar el archivo en el disco
+            with open('osb_enftransm_mpox_06092024.csv', 'wb') as file:
+                file.write(file_response.content)
+            print('Archivo descargado y guardado como osb_enftransm_mpox_06092024.csv')
+        else:
+            print('Error al descargar el archivo. Código de estado:', file_response.status_code)
